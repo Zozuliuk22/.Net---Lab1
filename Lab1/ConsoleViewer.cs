75f2df3b7febfbba6d988f1ab902537fb74b11ee
Viktor Zozuliuk <zozulyuk22@gmail.com>
2022-06-09 01:04:26 +0300

    Refactored viewer

--- Lab1/ConsoleViewer.cs
+++ Lab1/ConsoleViewer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using BuildingsCollection;
 using BuildingsCollection.Enums;
 using Application.Properties;
@@ -10,21 +11,31 @@ namespace Application
     {
         public static void ShowAllBlocks(IEnumerable<Block> blocks)
         {
-            Console.WriteLine("All Blocks:");
+            if (blocks != null && blocks.Any())
+            {
+                Console.WriteLine("All Blocks:");
 
-            foreach (var block in blocks)
-                Console.WriteLine($"\t{block}");
+                foreach (var block in blocks)
+                    Console.WriteLine($"\t{block}");
+            } 
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowCityFullInfo(Dictionary<string, List<House>> info)
         {
-            foreach(var block in info)
+            if(info != null && info.Any())
             {
-                Console.WriteLine($"Block: {block.Key}");
+                foreach (var block in info)
+                {
+                    Console.WriteLine($"Block: {block.Key}");
 
-                foreach (var house in block.Value)
-                    Console.WriteLine($"\tHouse: {house}");
+                    foreach (var house in block.Value)
+                        Console.WriteLine($"\tHouse: {house}");
+                }
             }
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowCityArea(double value)
@@ -40,46 +51,71 @@ namespace Application
 
         public static void ShowHousesByTypes(Dictionary<ProjectTypes, IEnumerable<House>> info)
         {
-            foreach (var type in info)
+            if(info != null && info.Any())
             {
-                Console.WriteLine($"Project type: {type.Key}");
+                foreach (var type in info)
+                {
+                    Console.WriteLine($"Project type: {type.Key}");
 
-                foreach (var house in type.Value)
-                    Console.WriteLine($"\t{house}");
+                    foreach (var house in type.Value)
+                        Console.WriteLine($"\t{house}");
+                }
             }
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowOneAndNineStoryHouses(Dictionary<int, IEnumerable<House>> info)
         {
-            foreach (var amount in info)
+            if(info != null && info.Any())
             {
-                Console.WriteLine($"{amount.Key}-story houses: ");
+                foreach (var amount in info)
+                {
+                    Console.WriteLine($"{amount.Key}-story houses: ");
 
-                foreach (var house in amount.Value)
-                    Console.WriteLine($"\t{house}");
+                    foreach (var house in amount.Value)
+                        Console.WriteLine($"\t{house}");
+                }
             }
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowNewHouses(IEnumerable<House> houses)
         {
-            Console.WriteLine("New houses:");
-            foreach (var house in houses)
-                Console.WriteLine($"\t{house}");
+            if(houses != null && houses.Any())
+            {
+                Console.WriteLine("New houses:");
+                foreach (var house in houses)
+                    Console.WriteLine($"\t{house}");
+            }   
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowHappyHouses(IEnumerable<House> houses)
         {
-            Console.WriteLine("Happy houses:");
-            foreach (var house in houses)
-                Console.WriteLine($"\t{house}");
+            if(houses!= null && houses.Any())
+            {
+                Console.WriteLine("Happy houses:");
+                foreach (var house in houses)
+                    Console.WriteLine($"\t{house}");
+            }
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowSpecificTypes(IEnumerable<ProjectTypes> types)
         {
-            Console.WriteLine(ConsoleTexts.SpecificTypesLabel);
+            if(types != null && types.Any())
+            {
+                Console.WriteLine(ConsoleTexts.SpecificTypesLabel);
 
-            foreach(var type in types)
-                Console.WriteLine($"\t{type}");
+                foreach (var type in types)
+                    Console.WriteLine($"\t{type}");
+            }
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowAdministrationAddress(string houseCode, string result)
@@ -90,18 +126,28 @@ namespace Application
 
         public static void ShowTheBiggestBlockInfo(IEnumerable<House> block)
         {
-            Console.WriteLine(ConsoleTexts.TheBiggestBlockLabel);
+            if(block!= null && block.Any())
+            {
+                Console.WriteLine(ConsoleTexts.TheBiggestBlockLabel);
 
-            foreach(var house in block)
-                Console.WriteLine($"\t{house}");
+                foreach (var house in block)
+                    Console.WriteLine($"\t{house}");
+            }
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowTopTenOldestHouses(IEnumerable<House> top)
         {
-            Console.WriteLine("Top 10 consists of:");
+            if(top!= null && top.Any())
+            {
+                Console.WriteLine("Top 10 consists of:");
 
-            foreach (var house in top)
-                Console.WriteLine($"\t{house}");
+                foreach (var house in top)
+                    Console.WriteLine($"\t{house}");
+            }
+            else
+                Console.WriteLine("Result: No one.");
         }
 
         public static void ShowPercentOfHighRise(string codeBlock, int percent)
@@ -112,15 +158,21 @@ namespace Application
 
         public static void ShowBlocksWithMultiEntrencesHouses(IEnumerable<Block> blocks)
         {
-            Console.WriteLine(ConsoleTexts.MultiEntrencesHousesLabel);
+            if(blocks != null && blocks.Any())
+            {
+                Console.WriteLine(ConsoleTexts.MultiEntrencesHousesLabel);
 
-            foreach(var block in blocks)
-                Console.WriteLine($"\t{block}");
+                foreach (var block in blocks)
+                    Console.WriteLine($"\t{block}");
+            }            
         }
 
         public static void ShowBlocksGameResult(Block block)
         {
-            Console.WriteLine($"\tResult: {block}");
+            if(block is null)
+                Console.WriteLine($"\tResult: No one");
+            else
+                Console.WriteLine($"\tResult: {block}");
         }
     }
 }
